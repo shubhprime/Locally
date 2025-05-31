1d9c2794087f68b0c254a5b2448d083077b54f08
Blue Hermes <shreyankumar94@gmail.com>
2025-05-31 11:43:33 +0530

    Uncommenting Redis Password Env Var

--- locally-user/src/main/java/com/locally/backend/config/SecurityConfig.java
+++ locally-user/src/main/java/com/locally/backend/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         exception.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/user/signup", "/api/auth/user/login", "/api/token/refresh", "/api/auth/user/logout", "/api/v1/password/forgot-password", "/api/v1/password/verify-otp", "/api/v1/password/reset-password").permitAll()
+                        .requestMatchers("/api/auth/user/signup", "/api/auth/user/login", "/api/auth/user/logout", "/api/v1/password/forgot-password", "/api/v1/password/verify-otp", "/api/v1/password/reset-password").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
